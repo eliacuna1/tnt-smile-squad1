b0dbdb040f39fe2b8ea488178377b329852ca220
Eli Acuna <eliacuna@Mac.lan>
2026-03-30 00:44:12 -0500

    Updates

--- assets/arrow-up-right-CjvaS_Ay.js
+++ assets/arrow-up-right-CjvaS_Ay.js
@@ -1,4 +1,4 @@
-import{a as o}from"./index-NB7ckFBc.js";/**
+import{a as o}from"./index-DZLfwyCk.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
